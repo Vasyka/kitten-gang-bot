07fabc1ff6332668d3a921d48c88e9af2802e8b4
Vasyka <vvanast@yandex.ru>
2023-12-10 11:27:49 +0300

    README

--- bot.py
+++ bot.py
@@ -8,42 +8,44 @@ import database
 from commands import *
 
 class KittenGangBot(discord.Client):
-    def __init__(
-        self,
-	    *args,
-        web_client: ClientSession,
-        intents: Optional[discord.Intents] = None,
-        testing_guild_id: Optional[int] = None,
-    ):
-        """Client initialization."""
-        if intents is None:
-            intents = discord.Intents.default()
-        intents.members = True
+	def __init__(
+		self,
+		*args,
+		web_client: ClientSession,
+		intents: Optional[discord.Intents] = None,
+		testing_guild_id: Optional[int] = None,
+	):
+		"""Client initialization."""
+		if intents is None:
+			intents = discord.Intents.default()
+		intents.members = True
 
-        super().__init__(intents=intents)
-        self.web_client = web_client
-        self.testing_guild_id = testing_guild_id
-        self.tree = app_commands.CommandTree(self)
+		super().__init__(intents=intents)
+		self.web_client = web_client
+		self.testing_guild_id = testing_guild_id
+		self.tree = app_commands.CommandTree(self)
 
-    async def on_ready(self):
-    	# Connection
-        await self.wait_until_ready()
-        print(f'Connected! Logged on as {self.user}!')
+	async def on_ready(self):
+		""" Connection """
+		await self.wait_until_ready()
+		print(f'Connected! Logged on as {self.user}!')
 
 
-    async def setup_hook(self) -> None:
-    	"""Setup Hook."""
-    	await database.bot_db.init_db_and_tables()
+	async def setup_hook(self) -> None:
+		"""Setup"""
+		await database.bot_db.init_db_and_tables()
 
-    	self.tree.add_command(hello)
-    	await self.tree.sync()
+		self.tree.add_command(hello)
+		self.tree.add_command(help)
 
-    	if self.testing_guild_id is not None:
-    		try:
-    			testing_guild = discord.Object(self.testing_guild_id)
-    			testing_guild.owner_id = (await self.fetch_guild(testing_guild.id)).owner_id
-    			await database.bot_db.add_a_guild(discord_guild_id=testing_guild.id, guild_owner_id=testing_guild.owner_id)
-    			self.tree.copy_global_to(guild=testing_guild)
-    			await self.tree.sync(guild=testing_guild)
-    		except discord.errors.Forbidden:
-    			print("Bot is not in the testing guild.")
+		await self.tree.sync()
+
+		if self.testing_guild_id is not None:
+			try:
+				testing_guild = discord.Object(self.testing_guild_id)
+				testing_guild.owner_id = (await self.fetch_guild(testing_guild.id)).owner_id
+				await database.bot_db.add_a_guild(discord_guild_id=testing_guild.id, guild_owner_id=testing_guild.owner_id)
+				self.tree.copy_global_to(guild=testing_guild)
+				await self.tree.sync(guild=testing_guild)
+			except discord.errors.Forbidden:
+				print("Bot is not in the testing guild.")
--- commands.py
+++ commands.py
@@ -8,13 +8,18 @@ from discord import app_commands
 logger = logging.getLogger("discord")
 
 @app_commands.command(name = "hello", description = 'Поздороваться')
-async def hello(interaction: discord.Interaction) -> None:
+async def hello(interaction: discord.Interaction):
 	# slash hello command
 
     guild=interaction.guild
-    #green_candy = discord.utils.get(guild.emojis, name='candy1')
     
-    embed = discord.Embed(title = 'Привет котенок! :smile_cat:', description="", color=0xff6f00)
-    embed.set_footer(text="Хехе")
+    embed = discord.Embed(title = 'Привет!', description="", color=0xff6f00)
 
-    await interaction.response.send_message(embed=embed, ephemeral=True)
+    await interaction.response.send_message(embed=embed, ephemeral=False)
+
+
+@app_commands.command(name = "help", description = 'Посмотреть список команд')
+async def help(interaction: discord.Interaction):
+    with open('help.txt', 'r') as f:
+        info = f.read()
+    await interaction.response.send_message(info, ephemeral=True)
